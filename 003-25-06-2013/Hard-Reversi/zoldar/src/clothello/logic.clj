1f0c614ba23fd4e995ef47f3f49855d4ab28949c
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-06-26 22:54:16 +0200

    First working version of game's logic.

--- 003-25-06-2013/Hard-Reversi/zoldar/src/clothello/logic.clj
+++ 003-25-06-2013/Hard-Reversi/zoldar/src/clothello/logic.clj
@@ -25,25 +25,49 @@
 
 (def empty-board (vec (repeat 8 (vec (repeat 8 :empty)))))
 
+(def offsets (for [x (range -1 2) y (range -1 2) 
+                   :when (not-every? zero? [x y])] [x y]))
+
 (defn put-at [board piece [x y]]
   (assoc-in board [x y] piece))
 
 (defn get-at [board [x y]]
   (get-in board [x y]))
 
-(def basic-board
+(def classic-board
   (-> empty-board
       (put-at :light [3 3])
       (put-at :light [4 4])
       (put-at :dark [3 4])
       (put-at :dark [4 3])))
 
-(defn valid-move? [side board [x y]]
-  true)
+(defn generate-line [start offset]
+  (take-while (fn [position] (every? #(< -1 % 8) position)) 
+              (iterate #(->> % (map + offset) vec) start)))
+
+(defn get-flippable-positions [side board [_ & line]]
+  (let [fields (map (partial get-at board) line)
+        flip-check-fn (fn [position] (and (not= side position) 
+                                          (not= :empty position)))
+        flip-candidates (take-while flip-check-fn fields)
+        [last-after-flips & _] (drop-while flip-check-fn fields)]
+    (when (and (seq flip-candidates) (= last-after-flips side))
+      (take (count flip-candidates) line))))
+
+(defn get-pieces-to-flip [side board move]
+  (let [lines (map (partial generate-line move) offsets)]
+    (mapcat (partial get-flippable-positions side board) lines)))
+
+(defn valid-move? [side board move]
+  (and (= (get-at board move) :empty) 
+       (seq (get-pieces-to-flip side board move))))
 
 (defn make-move [side board [x y :as move]]
   (when (valid-move? side board move)
-    board))
+    (let [pieces-to-flip (get-pieces-to-flip side board move)]
+      (reduce (fn [board position] (put-at board side position)) 
+              (put-at board side move) 
+              pieces-to-flip))))
 
 (defn get-valid-moves [side board]
   (let [coordinates (for [x (range 8) y (range 8)] [x y])]
@@ -76,7 +100,8 @@
   (partial ai-player side [] decision-fn))
 
 (defn make-random-decision [side history board]
-  (rand-nth (get-valid-moves side board)))
+  (when-let [valid-moves (seq (get-valid-moves side board))] 
+    (rand-nth valid-moves)))
 
 (defn create-random-ai-player [side]
   (create-ai-player side make-random-decision))
@@ -90,10 +115,10 @@
   (let [keyword-fn (fn [suffix] (keyword (str (name side) suffix)))
         board-keyword (keyword-fn "-board")
         move-fn-keyword (keyword-fn "-move-fn")
-        [move new-move-fn] (move-fn board)]
-    {board-keyword (make-move board side move) move-fn-keyword new-move-fn}))
+        {:keys [move move-fn]} (move-fn board)]
+    {board-keyword (make-move side board move) move-fn-keyword move-fn}))
 
-(defn game-step [{:keys [board dark-move-fn light-move-fn] :as game-stage}]
+(defn game-step [{:keys [board dark-move-fn light-move-fn]}]
   (when-not (game-finished? board)
     (let [{:keys [dark-board dark-move-fn]} (make-move-or-fail :dark dark-move-fn board)
           {:keys [light-board light-move-fn]} (make-move-or-fail :light light-move-fn 
@@ -108,4 +133,3 @@
                                         dark-player-constructor 
                                         light-player-constructor)]
     (cons game-stage (lazy-seq (game-step game-stage)))))
-
